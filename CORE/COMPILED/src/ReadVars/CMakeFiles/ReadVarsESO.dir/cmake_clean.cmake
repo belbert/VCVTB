1c57c209e3408a8600d822641effff6de2882052
belbert <35769429+belbert@users.noreply.github.com>
2019-01-08 09:53:40 +0100

    Add files via upload

---
+++ CORE/COMPILED/src/ReadVars/CMakeFiles/ReadVarsESO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReadVarsESO.dir/ReadVarsESO.f90.o"
+  "../../Products/ReadVarsESO.pdb"
+  "../../Products/ReadVarsESO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/ReadVarsESO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
