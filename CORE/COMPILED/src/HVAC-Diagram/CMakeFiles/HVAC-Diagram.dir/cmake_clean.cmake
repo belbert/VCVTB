818d28d1f393fee15bc1157ddb155df140bef594
belbert <35769429+belbert@users.noreply.github.com>
2019-01-08 09:56:19 +0100

    Add files via upload

---
+++ CORE/COMPILED/src/HVAC-Diagram/CMakeFiles/HVAC-Diagram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HVAC-Diagram.dir/HVAC-Diagram-Main.f90.o"
+  "../../Products/HVAC-Diagram.pdb"
+  "../../Products/HVAC-Diagram"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/HVAC-Diagram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
