3795ae8157b12e4e8a31ee8571b5a3844ec40e0d
belbert <35769429+belbert@users.noreply.github.com>
2019-01-08 09:39:21 +0100

    Add files via upload

---
+++ CORE/COMPILED/src/EnergyPlus/CMakeFiles/energyplusapi.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/energyplusapi.dir/CommandLineInterface.cc.o"
+  "CMakeFiles/energyplusapi.dir/EnergyPlusPgm.cc.o"
+  "../../Products/libenergyplusapi.pdb"
+  "../../Products/libenergyplusapi.so"
+  "../../Products/libenergyplusapi.so.8.8.0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/energyplusapi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
